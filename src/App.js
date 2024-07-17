b3ec2ca71bce9cd8e3558506303a193ea1e7331a
aswinss18 <aswinartography18@gmail.com>
2024-07-17 22:36:43 +0530

    Lifting of state and setState function is done to communicate data between Form and PackingList (siblings).

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,16 +23,17 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away 🎒</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
 
     //guard clause
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-
+    onAddItems(newItem);
     //reset form
     setDescription("");
     setQuantity(1);
@@ -55,11 +60,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
